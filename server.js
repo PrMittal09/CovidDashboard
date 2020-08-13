ed864443d1b22e996fec3e97771628a0a1930ec0
PrMittal09 <prateekmittal075@gmail.com>
2020-08-14 01:59:00 +0530

    using compression middleware and gzip build

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-const compression = require('compression');
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Dashboard'));
-app.use(compression());
 
 app.get('*.js', function(req, res, next) {
     req.url = req.url + '.gz';
-    res.header('Content-Encoding', 'gzip');
-    res.set('Content-Type', 'text/javascript');
+    res.set({
+        'Content-Encoding':'gzip',
+        'Content-Type':'text/javascript'
+    });
     next();
 });
 
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/Dashboard'));
+
 app.get('/*', function(req,res) {
-    
 res.sendFile(path.join(__dirname+'/dist/Dashboard/index.html'));
 });
 
